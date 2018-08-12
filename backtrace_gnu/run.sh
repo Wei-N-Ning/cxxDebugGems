d82a244c398a8bdc6fe074549d174882e33def1d
Wei Ning <nwcgnw@gmail.com>
2018-08-12 18:43:43 +1200

    - update backtrace_gnu example

--- backtrace_gnu/run.sh
+++ backtrace_gnu/run.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 
-mkdir -p /tmp/sut
+setUp() {
+    mkdir -p /tmp/sut
+    rm -f /tmp/sut/*.so
+    rm -f /tmp/sut/caller
+}
 
 # $1: callee cxxflags
 # $2: caller cxxflags
 run() {
-    c++ ${1} -std=c++14 -rdynamic -shared -fPIC -o /tmp/sut/libcallee.so ./callee.cpp
+    # see :
+    # man ld
+    # -l namespec
+    # --library=namespec
+    # Add the archive or object file specified by namespec to the list of files to
+    # link.  This option may be used any number of times.  If namespec is of the form
+    # :filename, ld will search the library path for a file called filename, otherwise
+    # it will search the library path for a file called libnamespec.a.
+    c++ ${1} -std=c++14 -rdynamic -shared -fPIC -o /tmp/sut/callee.so ./callee.cpp
     c++ ${2} -std=c++14 -rdynamic ./caller.cpp -o /tmp/sut/caller \
-    -Wl,-lcallee \
+    -Wl,-l:callee.so \
     -Wl,-L/tmp/sut \
     -Wl,-rpath=/tmp/sut
     /tmp/sut/caller
