e6056dcf338b1bf0511aaf6979a1faeca09e52c1
Wei Ning <nwcgnw@gmail.com>
2018-09-04 07:13:21 +1200

    - improve data race example

--- data_races/obvious_interleaving_fixed.cpp
+++ data_races/obvious_interleaving_fixed.cpp
@@ -9,50 +9,29 @@
 #include <vector>
 #include <atomic>
 
-// a cheap method to "fix" the data race:
-// apply only the atomic operations on the mutable data
-
-// NOTE acc is still concurrently read/written by multiple
-// threads but setter getter now are atomic
-
-// here is a good read:
-// https://stackoverflow.com/questions/30549327/best-way-to-print-information-when-debugging-a-race-condition
-// why GDB would be useless in this case
-
-// No, it's much worse. Every breakpoint that is hit in GDB triggers the following chain of events:
-//context switch from running thread to GDB
-//GDB stops all other threads (assuming default all-stop mode)
-//GDB evaluates breakpoint commands
-//GDB resumes all threads (this is itself a complicated multi-step process, which I would not go into here).
-// I second the ThreadSanitizer recommendation by Christopher Ian Stern.
-
+// to fix it the design must be changed
 
 struct Accessor {
     explicit Accessor(std::atomic<int>* data)
         :m_data(data) {
     }
 
-    void set(int v) {
-        *m_data = v;
-    }
-
-    int get() {
-        return *m_data;
+    void incr() {
+        (*m_data)++;
     }
 
     std::atomic<int>* m_data = nullptr;
 };
 
 int main() {
-    std::atomic<int> a{0xDEAD};
+    std::atomic<int> a{0};
     auto worker = [&](int v) -> int {
         Accessor acc(&a);
         for (int i = 1000 - v * 123; i--; ) {
         }
-        acc.set(v);
+        acc.incr();
         for (int i = 1000 - v * 231; i--; ) {
         }
-        int expected = acc.get();
         return v;
     };
 
@@ -67,6 +46,8 @@ int main() {
         th.join();
     }
 
+    // the result is 8 because there are 8 workers;
+    // each worker increments the mutable data by 1
     std::cout << a << std::endl;
 
     return 0;
